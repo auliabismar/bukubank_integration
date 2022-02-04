47792a484d6bec28472714283533299494484b41
auliabismar <auliabismar@yahoo.com>
2022-02-04 16:18:08 +0700

    Pertama

---
+++ bukubank_integration/bukubank_integration/doctype/bukubank_log/bukubank_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Sekolah360 and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Bukubank Log', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ bukubank_integration/bukubank_integration/doctype/bukubank_raw/test_bukubank_raw.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Sekolah360 and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestBukubankRaw(unittest.TestCase):
+	pass
